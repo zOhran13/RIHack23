6e564b9092c3c924dc5bb6317d408760bec1dd05
zlata <zohran1@etf.unsa.ba>
2023-10-07 23:46:19 +0200

    Logout

--- FitFam/lib/src/screens/profile.dart
+++ FitFam/lib/src/screens/profile.dart
@@ -1,7 +1,10 @@
 import 'package:fitfam/src/common_widgets/custom_pedometer.dart';
+import 'package:fitfam/src/screens/login.dart';
 import 'package:fitfam/src/utils/global_variables.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+
+import '../common_widgets/custom_alert_dialog.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -20,9 +23,35 @@ class ProfileScreen extends StatelessWidget {
             Container(
                 padding: const EdgeInsets.only(top: 30.0),
                 margin: const EdgeInsets.only(left: 300.0),
-                child: SvgPicture.asset(GlobalVariables.logoutIcon,
-                    colorFilter:
-                        const ColorFilter.mode(Colors.white, BlendMode.srcIn)))
+                child: InkWell(
+                  onTap: () {
+                          showDialog<String>(
+                            context: context,
+                            builder: (BuildContext context) =>
+                                const CustomAlertDialog(
+                              text1: 'Logout',
+                              text2: 'Are you sure you want to Logout?',
+                            ),
+                          ).then((value) {
+                    if (value == 'OK') {
+                    
+                      
+
+                      
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(
+                          builder: (BuildContext context) => LoginScreen(),
+                        ),
+                      );
+                    }
+                  });
+                },
+
+                  child: SvgPicture.asset(GlobalVariables.logoutIcon,
+                  
+                      colorFilter:
+                          const ColorFilter.mode(Colors.white, BlendMode.srcIn)),
+                ))
             //const Text('Level 1'),
           ]),
         ),
